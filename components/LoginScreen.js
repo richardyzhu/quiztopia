bb2ab399e6c72e5ffc0666a293cbf1bae4325ad9
Rijrya <richard.yunqi.zhu@gmail.com>
2023-01-29 03:39:47 -0500

    Added Discord Auth

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { Client, Account, Id } from "appwrite";
 
 const LoginScreen = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = () => {
+    const client = new Client()
+      .setEndpoint('https://cloud.appwrite.io/v1')
+      .setProject('63d5326b15e8272efde2');
+    
+    const account = new Account(client);
+
+    account.createOAuth2Session('discord',
+    'https://discord.com/api/oauth2/authorize?client_id=1069168038080954368&redirect_uri=https%3A%2F%2Fcloud.appwrite.io%2Fv1%2Faccount%2Fsessions%2Foauth2%2Fcallback%2Fdiscord%2F63d5326b15e8272efde2&response_type=code&scope=identify',
+    );
+
     console.log(`Username: ${username}, Password: ${password}`);
   };
 
