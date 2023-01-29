3c4a4d2ba6d649427ff8f82d4304a70b9b6b33f2
sammyyyyy1 <115298250+sammyyyyy1@users.noreply.github.com>
2023-01-28 22:27:17 -0500

    removed question randomizer

--- components/QuestionManager.js
+++ components/QuestionManager.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { useState } from 'react';
-import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
 import Question from './Question'
 
 class QuestionManager extends React.Component {
@@ -11,18 +9,7 @@ class QuestionManager extends React.Component {
             answers: ['Go to class', 'Eat lunches', 'Take a dump', 'None of the above'],
             correctAnswer: 'Go to class'
         }
-        this.updateQuestion = this.updateQuestion.bind(this);
     }
-    updateQuestion = () => {
-        // code to generate new question and answers
-        console.log("updating question");
-        const randomNumber = Math.floor(Math.random() * 10);
-        this.setState({ 
-            questionText: 'This is a new question!',
-            answers: [randomNumber, 'Answer 2', 'Answer 3', 'Answer 4'],
-            correctAnswer: 'Answer 3',
-    }
-    )}
         
     render() {
         return (
@@ -30,7 +17,6 @@ class QuestionManager extends React.Component {
                 questionText={this.state.questionText}
                 answers={this.state.answers}
                 correctAnswer={this.state.correctAnswer}
-                updateQuestion={this.updateQuestion}
             />
         )
     }
